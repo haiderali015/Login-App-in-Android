eb585b33627652d10d3bff1c02ff9b5b5b22ed51
haider ali <ha347278@gmail.com>
2022-07-22 20:20:35 +0500

    commited changes

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,13 +13,16 @@ import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText username,password,cnfrmpass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
+         username =  findViewById(R.id.username);
+         password =  findViewById(R.id.password);
+         cnfrmpass = findViewById(R.id.cnfrmpass);
+        DBHelper Db= new DBHelper(this);
 
         Button loginbtn = (Button) findViewById(R.id.loginbtn);
         Button register = (Button) findViewById(R.id.register);
@@ -43,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String uname=username.getText().toString();
+                String pass=password.getText().toString();
+                String repass=cnfrmpass.getText().toString();
 
             }
         });
